cded4694da03a953374e5255a89face87467cf34
Abhishek Shah <abhishekshah3@gmail.com>
2024-04-06 10:27:17 +0530

    init

--- main.rs
+++ main.rs
@@ -1,7 +1,8 @@
 use anyhow::{Context, Result};
 use std::env;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let city = args.get(1);
     if args.len() != 2 || args.get(1).is_none() {
@@ -10,14 +11,22 @@ fn main() -> Result<()> {
     }
     let city = city.unwrap().to_owned();
     println!("Fetching weather for {}", city);
-    fetch_weather(city)
+    fetch_weather(city).await
 }
 
-fn fetch_weather(city: String) -> Result<()> {
+async fn fetch_weather(city: String) -> Result<()> {
+    use html2text::config;
     let url = format!("https://wttr.in/{}", city);
-    std::process::Command::new("curl")
-        .arg(url)
-        .spawn()
-        .context("Failed to fetch weather command")?;
+    let bytes = reqwest::get(&url)
+        .await
+        .context("Failed to make network request")?
+        .bytes()
+        .await
+        .context("Failed to read into bytes")?;
+    let mut reader = std::io::Cursor::new(bytes);
+    let s = config::rich()
+        .string_from_read(&mut reader, 150)
+        .context("Render failed")?;
+    println!("{s}");
     Ok(())
 }
